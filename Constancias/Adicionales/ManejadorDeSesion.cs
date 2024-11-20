0566e18e5bea0f57d4d16ca182c16775106bfe27
Drawiel <daur0704@outlook.com>
2024-11-20 00:41:33 -0600

    Agregación de adicionales para modificar campos y sesión de usuario con Singleton

---
+++ Constancias/Adicionales/ManejadorDeSesion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constancias.Adicionales {
+    public class ManejadorDeSesion {
+        private static ManejadorDeSesion instancia;
+        private SesionDeUsuario usuario;
+
+        public static ManejadorDeSesion GetInstancia() {
+            if (instancia == null) {
+                instancia = new ManejadorDeSesion();
+            }
+            return instancia;
+        }
+
+        public void IniciarSesion(SesionDeUsuario usuario) {
+            this.usuario = usuario;
+        }
+
+        public SesionDeUsuario GetUsuario() {
+            return this.usuario;
+        }
+
+        public void SalirDeSesion() {
+            this.usuario = null;
+        }
+    }
+}
---
+++ Constancias/Adicionales/SesionDeUsuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constancias.Adicionales {
+    public class SesionDeUsuario {
+        private string nombreUsuario;
+        private string correo;
+
+        public string NombreUsuario { get { return nombreUsuario; } set { nombreUsuario = value; } }
+        public string Correo { get { return correo;  } set { correo = value;  } }
+    }
+}
